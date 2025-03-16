32cf9e52fb8a3e594d7eed06bfac74397b27c5a6
jiayizhang19 <j.zhang19@outlook.com>
2025-03-16 20:59:26 +0800

    add zhipu model

--- local_run/agent.py
+++ local_run/agent.py
@@ -2,6 +2,8 @@
 from browser_use.agent.service import Agent
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain_openai import ChatOpenAI
+from langchain_community.chat_models import ChatZhipuAI
+# from langchain_core.messages import AIMessage, HumanMessage, SystemMessage
 from pydantic import SecretStr
 from dotenv import load_dotenv
 import os
@@ -10,27 +12,37 @@ load_dotenv()
 
 def initialize_agent(task):
 
-    # llm = ChatOpenAI(
-    #     base_url='https://api.deepseek.com/v1',
-    #     model='deepseek-chat', 
-    #     api_key=SecretStr(os.getenv('DEEPSEEK_API_KEY'))
-    #     )
+    llm_deepseek = ChatOpenAI(
+        base_url='https://api.deepseek.com/v1',
+        model='deepseek-chat', 
+        api_key=SecretStr(os.getenv('DEEPSEEK_API_KEY'))
+        )
 
-    llm = ChatOpenAI(
+    llm_siliconflow = ChatOpenAI(
         base_url='https://api.siliconflow.cn/v1',
         model='Pro/deepseek-ai/DeepSeek-V3',
         api_key=SecretStr(os.getenv('SILICONFLOW_API_KEY'))
     )
     
-    llm = ChatGoogleGenerativeAI(
+    llm_gemini = ChatGoogleGenerativeAI(
         model='gemini-2.0-flash-exp', 
         api_key=SecretStr(os.getenv('GEMINI_API_KEY'))
-        )
+    )
+    
+    llm_zhipu = ChatZhipuAI(
+        base_url='https://api.zhipu.ai/v1',
+        model='glm-4', 
+        api_key=os.getenv('ZHIPU_API_KEY'),
+        # temperature=0.5
+        
+    )
+
     agent = Agent(
         task=task, 
-        llm=llm, 
+        llm=llm_zhipu, 
         use_vision=False, 
         save_conversation_path='../logs/conversation'
-    )
+        # controller=Controller()
+        )
     
     return agent
--- local_run/executor.py
+++ local_run/executor.py
@@ -2,6 +2,7 @@
 import asyncio
 from agent import initialize_agent
 from case import *
+from playwright.async_api import async_playwright
 
 
 async def execute(task):
@@ -11,8 +12,15 @@ async def execute(task):
     print('test result:',test_result)
     # print('Raw API Response:', json.dumps(test_result, indent=2, ensure_ascii=False)
 
+# # Dismiss the below function if run with playwright chromium
+# async def run_with_custom_chrome(task):
+#     async with async_playwright() as p:
+#         browser = await p.chromium.launch(headless=False,executable_path="G:\ProgramFiles\chromium-win64\chrome-win\chrome.exe")
+#         await execute(task)
+#         await browser.close()
+
 if __name__ == '__main__':
     asyncio.run(
-    # execute(other_task),
     execute(login_bfd_task)
+    # run_with_custom_chrome(other_task)
 )
